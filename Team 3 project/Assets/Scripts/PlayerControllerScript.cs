8e62f3450d990957fc7abc1b0024865baaa4bd4c
Doppler-Effect <17582973+Lizzie1177@users.noreply.github.com>
2023-05-09 10:58:42 -0400

    Coyote Timer

--- Team 3 project/Assets/Scripts/PlayerControllerScript.cs
+++ Team 3 project/Assets/Scripts/PlayerControllerScript.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal;
 
 public class PlayerControllerScript : MonoBehaviour
 {
@@ -22,6 +23,11 @@ public class PlayerControllerScript : MonoBehaviour
     [SerializeField]
     private float jumpHeight = 2.4f;
 
+    [SerializeField]
+    private float coyoteTimerStart = 3f;
+
+    private float coyoteTimer;
+
     private CharacterController controller;
 
     public Transform ground;
@@ -31,11 +37,17 @@ public class PlayerControllerScript : MonoBehaviour
     public LayerMask groundMask;
 
     private bool isGrounded;
-    // Start is called before the first frame update
+
+    //This is janky. I don't like it, but I can't figure out better way to make coyote time not allow for a double jump
+    private bool recentJump;
+
+    private float recentJumpTimer = 0.5f;
+
     void Awake()
     {
         controls = new PlayerControls();
         controller = GetComponent<CharacterController>();
+        coyoteTimer = coyoteTimerStart;
     }
 
     private void OnEnable()
@@ -51,20 +63,36 @@ public class PlayerControllerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Jump();
         Grav();
         PlayerMovement();
-        Jump();
+
+        if (recentJump)
+        {
+            recentJumpTimer -= Time.deltaTime;
+
+            if (recentJumpTimer < 0)
+            {
+                recentJumpTimer = 0.5f;
+                recentJump = false;
+            }
+        }
+
     }
+
+    //Basic movement functions
 
     private void Jump()
     {
-        if (controls.Player.Jump.triggered)
+        if (controls.Player.Jump.triggered && (isGrounded || coyoteTimer > 0))
         {
             velocity.y = MathF.Sqrt(jumpHeight * -2f * gravity);
-
+            coyoteTimer = 0f;
+            recentJump = true;
         }
     }
 
+    
     private void PlayerMovement()
     {
         move = controls.Player.Move.ReadValue<Vector2>();
@@ -76,6 +104,15 @@ public class PlayerControllerScript : MonoBehaviour
     private void Grav()
     {
         isGrounded = Physics.CheckSphere(ground.position, distanceToGround, groundMask);
+
+        if (isGrounded)
+        {
+            if (!recentJump)   coyoteTimer = coyoteTimerStart;
+        }
+        else
+        {
+            if (coyoteTimer > 0)    coyoteTimer -= Time.deltaTime;
+        }
 
         if (isGrounded && velocity.y < 0)
         {
