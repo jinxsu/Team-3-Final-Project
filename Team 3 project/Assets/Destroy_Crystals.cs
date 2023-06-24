c16cf814712893a2b698c39ffb2e6539df462a2b
StoicPuppy <Patmarler@me.com>
2023-06-23 22:43:06 -0400

    Fixed the Arena. Fixed the size of the models.
    The models are fixed. going to add more detail. for now a makeshift level is possible

--- Team 3 project/Assets/Destroy_Crystals.cs
+++ Team 3 project/Assets/Destroy_Crystals.cs
@@ -4,26 +4,17 @@ using UnityEngine;
 
 public class Destroy_Crystals : MonoBehaviour
 {
+    public bool TimeOver = false;
 
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            Destroy(this.gameObject, 2f);
+
+            Destroy(this.gameObject, 1f);
+            
         }
     }
     
