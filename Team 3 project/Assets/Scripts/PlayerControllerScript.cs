7d079966efa22e04f6c62d7fc51dbbef3a4b0e7a
myiqis9 <emv.rad@gmail.com>
2023-07-17 12:42:53 -0400

    Revert "Revert "Auto stash before revert of "Merge remote-tracking branch 'origin/Patrick' into Ema"""
    This reverts commit 5dcf07f1edd96867b66bd056c20646aa85be4508.

--- Team 3 project/Assets/Scripts/PlayerControllerScript.cs
+++ Team 3 project/Assets/Scripts/PlayerControllerScript.cs
@@ -339,13 +339,6 @@ public class PlayerControllerScript : MonoBehaviour
         {
             anim.SetTrigger("interact");
             fullbodyAnim.SetTrigger("interact");
-
-            RaycastHit hit;
-            if (Physics.Raycast(POV.transform.position, POV.transform.forward, out hit, 20, interactMask))
-            {
-                Debug.Log("Interacted!");
-                hit.transform.SendMessage("Interact");
-            }
         }
     }
 
