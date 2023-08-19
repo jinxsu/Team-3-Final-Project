50430c6c51be422683030c69a5dbc07bcfdb0c4d
Ema Radu <83728442+myiqis9@users.noreply.github.com>
2023-08-18 20:18:09 -0400

    yayyy

--- Team 3 project/Assets/Scripts/Traps/LighterTrapDetectionArea.cs
+++ Team 3 project/Assets/Scripts/Traps/LighterTrapDetectionArea.cs
@@ -5,15 +5,43 @@ using UnityEngine;
 public class LighterTrapDetectionArea : MonoBehaviour
 {
     [SerializeField]
-    LighterTrap ligherTrap;
+    LighterTrap lighterTrap;
+    private PlayerControllerScript player;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Boss")) ligherTrap.angelIsIn = true;
+        if (other.CompareTag("Boss")) lighterTrap.angelIsIn = true;
+
+        if(other.CompareTag("Player"))
+        {
+            player = other.GetComponent<PlayerControllerScript>();
+            player.canInteract = true;
+            player.intString = "This house looks flammable...";
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Boss")) ligherTrap.angelIsIn = false;
+        if (other.CompareTag("Boss")) lighterTrap.angelIsIn = false;
+
+        if (other.CompareTag("Player"))
+        {
+            player.canInteract = false;
+            player = null;
+        }
+    }
+
+    private void Update()
+    {
+        if(player != null)
+        {
+            if (GameObject.Find("Lighter Variant(Clone)") && player.controls.Player.Fire.triggered)
+            {
+                if (lighterTrap.angelIsIn) 
+                    player.intString = "I can't do that now! I'll burn myself!";
+                else 
+                    player.intString = "That thing isn't here yet! I'll set up a trap.";
+            }
+        }
     }
 }
